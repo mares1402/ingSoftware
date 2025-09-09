79ce28455bd2e46e1c8f70a88669c1e083cc2103
mares1402 <maresd1402@gmail.com>
2025-09-09 12:17:22 -0600

    Verision 1.3

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const conexion = require('./Back/conexion');
+const { registrarUsuario, verificarDuplicados } = require('./Back/singup');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/Front'));
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Servidor escuchando en puerto 3000');
+});
