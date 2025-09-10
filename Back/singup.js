7680ccd238cf213d08414adcfdb06336af54cee8
mares1402 <maresd1402@gmail.com>
2025-09-10 17:08:07 -0600

    Version 1.7.4

---
+++ Back/singup.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('.campo input, .campo select').forEach(elemento => {
+    const label = elemento.parentElement.querySelector('label');
+
+    const actualizarLabel = () => {
+        if (elemento.value.trim() !== '') {
+            label.classList.add('label-oculto');
+        } else {
+            label.classList.remove('label-oculto');
+        }
+    };
+
+    actualizarLabel();
+    elemento.addEventListener('input', actualizarLabel);
+    elemento.addEventListener('change', actualizarLabel);
+});
