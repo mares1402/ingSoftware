b627e38dbdf50236e8a549fb285676824c28fec3
mares1402 <maresd1402@gmail.com>
2025-12-01 17:46:04 -0600

    Reapply "diseño de editar cotizacion"
    This reverts commit 94ff1c5bc8ed95e0fbead1e50117e6bc45b9175e.

--- Back/admin-routes.js
+++ Back/admin-routes.js
@@ -1102,7 +1102,6 @@ router.post('/cotizaciones/:id/mark-returned', isAuthenticated, isAdmin, (req, r
   );
 });
 
-<<<<<<< HEAD
 // Eliminar detalle de cotización
 router.delete('/cotizaciones/:id/detalles/:idDetalle', isAuthenticated, isAdmin, (req, res) => {
   const { id, idDetalle } = req.params;
@@ -1216,8 +1215,6 @@ router.post('/cotizaciones/:id/productos/add', isAuthenticated, isAdmin, (req, r
     }
   );
 });
-=======
->>>>>>> parent of b77b2da (diseño de editar cotizacion)
 
 // --- Panel de administración ---
 router.get('/panel/:archivo', isAuthenticated, isAdmin, (req, res) => {
